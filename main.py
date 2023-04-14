df7b4438b8c1efd9fa846bba9d324f9144d2bd5b
Blackskies <gumpena.ravikumar@gmail.com>
2023-04-14 19:32:16 +0530

    App Server config change

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = Dash(__name__, use_pages=True)
 app.layout = html.Div([html.H1('Dota 2 stats'), dash.page_container])
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=80, host="0.0.0.0")
