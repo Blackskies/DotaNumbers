3a63960a7f4957fce7550b3c745ef757c023cb32
Blackskies <gumpena.ravikumar@gmail.com>
2023-04-14 19:21:54 +0530

    my initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from dash import Dash, html, dcc
+import dash
+
+app = Dash(__name__, use_pages=True)
+
+app.layout = html.Div([html.H1('Dota 2 stats'), dash.page_container])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
